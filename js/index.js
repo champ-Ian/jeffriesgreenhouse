7a2fee3cef587b5ad899cdfb453189a8013991ad
ianbai <107971887+champ-Ian@users.noreply.github.com>
2024-08-15 18:22:39 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,15 +4,20 @@ function start(){
     document.querySelector('.house2').style.display = 'none'
     document.querySelector('.house3').style.display = 'none'
     document.querySelector('.house4').style.display = 'none'
+    document.querySelector('.house5').style.display = 'none'
+    document.querySelector('.house6').style.display = 'none'
+    document.querySelector('.house7').style.display = 'none'
+    document.querySelector('.house8').style.display = 'none'
+    document.querySelector('.house9').style.display = 'none'
     document.querySelector('.first-button').style.display = 'none'
 }
 
 function minus(){
     if (currentOne==2){
         currentOne -= 1
+        document.querySelector('.first-button').style.display = 'none'
         document.querySelector('.house1').style.display = 'block'
         document.querySelector('.house2').style.display = 'none'
-        document.querySelector('.first-button').style.display = 'none'
     } else if (currentOne==3){
         currentOne -= 1
         document.querySelector('.house2').style.display = 'block'
@@ -21,8 +26,28 @@ function minus(){
         currentOne -= 1
         document.querySelector('.house3').style.display = 'block'
         document.querySelector('.house4').style.display = 'none'
+    }else if (currentOne==5){
+        currentOne -= 1
+        document.querySelector('.house4').style.display = 'block'
+        document.querySelector('.house5').style.display = 'none'
+    }else if (currentOne==6){
+        currentOne -= 1
+        document.querySelector('.house5').style.display = 'block'
+        document.querySelector('.house6').style.display = 'none'
+    }else if (currentOne==7){
+        currentOne -= 1
+        document.querySelector('.house6').style.display = 'block'
+        document.querySelector('.house7').style.display = 'none'
+    }else if (currentOne==8){
+        currentOne -= 1
+        document.querySelector('.house7').style.display = 'block'
+        document.querySelector('.house8').style.display = 'none'
+    }else if (currentOne==9){
+        currentOne -= 1
+        document.querySelector('.house8').style.display = 'block'
+        document.querySelector('.house9').style.display = 'none'
         document.querySelector('.second-button').style.display = 'block'
-    } 
+    }
 }
 
 function plus(){
@@ -39,6 +64,26 @@ function plus(){
         currentOne += 1
         document.querySelector('.house3').style.display = 'none'
         document.querySelector('.house4').style.display = 'block'
+    }else if (currentOne==4){
+        currentOne += 1
+        document.querySelector('.house4').style.display = 'none'
+        document.querySelector('.house5').style.display = 'block'
+    }else if (currentOne==5){
+        currentOne += 1
+        document.querySelector('.house5').style.display = 'none'
+        document.querySelector('.house6').style.display = 'block'
+    }else if (currentOne==6){
+        currentOne += 1
+        document.querySelector('.house6').style.display = 'none'
+        document.querySelector('.house7').style.display = 'block'
+    }else if (currentOne==7){
+        currentOne += 1
+        document.querySelector('.house7').style.display = 'none'
+        document.querySelector('.house8').style.display = 'block'
+    }else if (currentOne==8){
+        currentOne += 1
+        document.querySelector('.house8').style.display = 'none'
+        document.querySelector('.house9').style.display = 'block'
         document.querySelector('.second-button').style.display = 'none'
     } 
 }
