a420bd65004667b9bdb93c3795547084cd5ebcf4
ianbai <107971887+champ-Ian@users.noreply.github.com>
2025-04-18 14:11:25 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,98 +1,35 @@
-currentOne = 1;
+// Get the modal and buttons
+const triggerImage = document.getElementById("triggerImage");
+const openPopupBtn = document.getElementById("openPopup");
+const popupSection = document.getElementById("popupsection");
+const backBtn = document.getElementById("backButton")
 
-function start(){
-    document.querySelector('.house2').style.display = 'none'
-    document.querySelector('.house3').style.display = 'none'
-    document.querySelector('.house4').style.display = 'none'
-    document.querySelector('.house5').style.display = 'none'
-    document.querySelector('.house6').style.display = 'none'
-    document.querySelector('.house7').style.display = 'none'
-    document.querySelector('.house8').style.display = 'none'
-    document.querySelector('.house9').style.display = 'none'
-    document.querySelector('.house10').style.display = 'none'
-    document.querySelector('.first-button').style.display = 'none'
-}
+openPopupBtn.addEventListener("click", () => {
+  popupSection.classList.add('show');
+  document.body.style.overflow = 'hidden'; // Lock main page scroll
+});
+triggerImage.addEventListener("click", () => {
+  popupSection.classList.add('show');
+  document.body.style.overflow = 'hidden'; // Lock main page scroll
+});
+backBtn.addEventListener("click", () => {
+  popupSection.classList.remove('show');
+  document.body.style.overflow = ''; // Restore main page scroll
+});
 
-function minus(){
-    if (currentOne==2){
-        currentOne -= 1
-        document.querySelector('.first-button').style.display = 'none'
-        document.querySelector('.house1').style.display = 'block'
-        document.querySelector('.house2').style.display = 'none'
-    } else if (currentOne==3){
-        currentOne -= 1
-        document.querySelector('.house2').style.display = 'block'
-        document.querySelector('.house3').style.display = 'none'
-    } else if (currentOne==4){
-        currentOne -= 1
-        document.querySelector('.house3').style.display = 'block'
-        document.querySelector('.house4').style.display = 'none'
-    }else if (currentOne==5){
-        currentOne -= 1
-        document.querySelector('.house4').style.display = 'block'
-        document.querySelector('.house5').style.display = 'none'
-    }else if (currentOne==6){
-        currentOne -= 1
-        document.querySelector('.house5').style.display = 'block'
-        document.querySelector('.house6').style.display = 'none'
-    }else if (currentOne==7){
-        currentOne -= 1
-        document.querySelector('.house6').style.display = 'block'
-        document.querySelector('.house7').style.display = 'none'
-    }else if (currentOne==8){
-        currentOne -= 1
-        document.querySelector('.house7').style.display = 'block'
-        document.querySelector('.house8').style.display = 'none'
-    }else if (currentOne==9){
-        currentOne -= 1
-        document.querySelector('.house8').style.display = 'block'
-        document.querySelector('.house9').style.display = 'none'
-    }else if (currentOne==10){
-        currentOne -= 1
-        document.querySelector('.house9').style.display = 'block'
-        document.querySelector('.house10').style.display = 'none'
-        document.querySelector('.second-button').style.display = 'block'
-    }
-}
 
-function plus(){
-    if (currentOne==1){
-        currentOne += 1
-        document.querySelector('.first-button').style.display = 'block'
-        document.querySelector('.house1').style.display = 'none'
-        document.querySelector('.house2').style.display = 'block'
-    } else if (currentOne==2){
-        currentOne += 1
-        document.querySelector('.house2').style.display = 'none'
-        document.querySelector('.house3').style.display = 'block'
-    } else if (currentOne==3){
-        currentOne += 1
-        document.querySelector('.house3').style.display = 'none'
-        document.querySelector('.house4').style.display = 'block'
-    }else if (currentOne==4){
-        currentOne += 1
-        document.querySelector('.house4').style.display = 'none'
-        document.querySelector('.house5').style.display = 'block'
-    }else if (currentOne==5){
-        currentOne += 1
-        document.querySelector('.house5').style.display = 'none'
-        document.querySelector('.house6').style.display = 'block'
-    }else if (currentOne==6){
-        currentOne += 1
-        document.querySelector('.house6').style.display = 'none'
-        document.querySelector('.house7').style.display = 'block'
-    }else if (currentOne==7){
-        currentOne += 1
-        document.querySelector('.house7').style.display = 'none'
-        document.querySelector('.house8').style.display = 'block'
-    }else if (currentOne==8){
-        currentOne += 1
-        document.querySelector('.house8').style.display = 'none'
-        document.querySelector('.house9').style.display = 'block'
-    }else if (currentOne==9){
-        currentOne += 1
-        document.querySelector('.house9').style.display = 'none'
-        document.querySelector('.house10').style.display = 'block'
-        document.querySelector('.second-button').style.display = 'none'
-    } 
-}
+
+//Show More Button
+const showMoreBtn = document.getElementById('showmorebutton');
+const closeBtn = document.getElementById('closebutton');
+const showMoreSection = document.getElementById('showmoresection');
+
+showMoreBtn.addEventListener('click', () => {
+  showMoreSection.classList.add('show');
+  document.body.style.overflow = 'hidden'; // Lock main page scroll
+});
+
+closeBtn.addEventListener('click', () => {
+  showMoreSection.classList.remove('show');
+  document.body.style.overflow = ''; // Restore main page scroll
+});
